f50f46f259a7885c3fd9f53239eda36d9adab8c5
Jeremy Grosser <jeremy@synack.me>
2018-12-24 04:22:53 -0800

    ensure startup is linked first

--- third_party/CMSIS/Device/ATMEL/SAMD21/build.mk
+++ third_party/CMSIS/Device/ATMEL/SAMD21/build.mk
@@ -3,4 +3,4 @@ LOCAL_DIR := $(GET_LOCAL_DIR)
 CFLAGS += -I$(LOCAL_DIR)/include \
 		  -I$(LOCAL_DIR)/source
 
-SRCS += $(LOCAL_DIR)/source/gcc/startup_samd21.c
+SRCS := $(LOCAL_DIR)/source/gcc/startup_samd21.c $(SRCS)
