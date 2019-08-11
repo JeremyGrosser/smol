72a35c4b94c7d6ab211ba9e53ad8a189859ece16
Jeremy Grosser <jeremy@synack.me>
2019-08-10 18:50:43 -0700

    Buttons

--- boards/microbit/board.c
+++ boards/microbit/board.c
@@ -2,6 +2,22 @@
 #include <platform.h>
 #include <board.h>
 
+gpio_t BUTTON_A = {
+    .num = 17,
+    .config = {
+        .direction = DIR_IN,
+        .pull = PULL_UP,
+    },
+};
+
+gpio_t BUTTON_B = {
+    .num = 26,
+    .config = {
+        .direction = DIR_IN,
+        .pull = PULL_UP,
+    },
+};
+
 int board_init() {
     platform_init();
     uart_setup(&CONSOLE);
